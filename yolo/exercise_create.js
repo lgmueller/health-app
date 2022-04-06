4765eb4714d63029d6c37e62bf34fd28fe093819
Lillian <lnose@umd.edu>
2022-04-05 23:12:22 -0400

    Adding table javascript

---
+++ yolo/exercise_create.js
@@ -0,0 +1,53 @@
+class Exercise {
+    constructor(sets, reps, my_name){
+        this.sets = sets;
+        this.reps = reps;
+        this.my_name = my_name;
+    }
+}
+
+
+var list = [];
+var text = [];
+
+function addToList() {
+    var set = document.getElementById('sets').value;
+    var rep = document.getElementById('reps').value;
+    var my_name = document.getElementById('nameField').value;
+    
+    if (set != null && rep != null && my_name != null){
+
+        list.push(new Exercise(set,rep,my_name));
+    } else {
+        alert("Sorry! Double check that you've filled out all of the fields.")
+        throw new Error("Invalid Input");
+    }
+
+    var table = document.getElementById('myTable');
+    var rowNode = document.createElement("tr");
+
+    var set_cellNode = document.createElement("td");
+    var set_textNode = document.createTextNode(set);
+    set_cellNode.appendChild(set_textNode);
+    rowNode.appendChild(set_cellNode);
+
+    var rep_cellNode = document.createElement("td");
+    var rep_textNode = document.createTextNode(rep);
+    rep_cellNode.appendChild(rep_textNode);
+    rowNode.appendChild(rep_cellNode);
+
+    var name_cellNode = document.createElement("td");
+    var name_textNode = document.createTextNode(my_name);
+    name_cellNode.appendChild(name_textNode);
+    rowNode.appendChild(name_textNode);
+    
+    table.appendChild(rowNode);
+    // for (let i = 0; i < list.length; i++){
+    //     // text_list.push(list[i].sets + '     ' + list[i].reps + '      ' + list[i].my_name + '<br />');
+    //     // var table = document.getElementById('myTable');
+        
+    // }
+
+
+    // document.getElementById('workout_table').innerHTML = work;
+}

c8278cfff08cc78ccb178195908bbe9c302e95d0
Ananya Uniyal <ananyauniyal@wireless-10-104-56-217.umd.edu>
2022-04-07 19:32:04 -0400

    new message

--- yolo/update_progress.js
+++ yolo/update_progress.js
@@ -139,9 +139,29 @@ function myExerFunction() {
     var get_curr_cal = localStorage.getItem("CurrCal");
     document.getElementById("new_curr_cal").innerHTML =  get_curr_cal + " Calories";
   }
+
+  function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+  
   /*
     NUTRITION PAGE
   */
+
+    
 
   new Chart(document.getElementById("bar-chart"), {
     type: 'bar',

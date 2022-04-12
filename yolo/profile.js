782ccc263253a681c20a694243e02a921d956569
Eric Fields <ericfields@Erics-MacBook-Pro.local>
2022-04-11 20:27:52 -0400

    updated keyboard

--- yolo/profile.js
+++ yolo/profile.js
@@ -1,5 +1,26 @@
 
 
+var click_count = 0
+
+function showKeyboard() {
+  var keyboard = document.getElementById("keyboard")
+  keyboard.style = "display: block"
+  click_count = 0
+}
+
+function hideKeyboard() {
+  var keyboard = document.getElementById("keyboard")
+  if (keyboard.style.display == "block" && click_count == 0) {
+    click_count += 1;
+  }
+  else {
+    click_count = 0
+    keyboard.style = "display: none"
+  }
+  
+}
+
+
 function loadInfo() {
 
     let name = localStorage.getItem("username")
